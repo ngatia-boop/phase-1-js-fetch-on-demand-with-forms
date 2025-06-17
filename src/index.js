4a651f89571fd16f2fb562e267cd71995cb797e3
ann ngatia <annngatia94@gmail.com>
2025-06-17 18:42:53 +0300

    fetch on demands with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // Stop the page from reloading
+    const input = document.querySelector("#searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("#movieDetails h4");
+        const summary = document.querySelector("#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
